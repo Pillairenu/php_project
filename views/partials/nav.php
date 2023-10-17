10498a239847d66cefec90737e32cd564fcbb848
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-10-17 16:59:01 -0700

    Create submenu in top nav

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -5,12 +5,13 @@
     <nav class="main-nav">
       <ul>
         <li><a href="index.php">Home</a></li>
-        <li class="dropdown"><a href="#" class="drpbtn">Services</a>
-            <!-- <div class="drpmenu">
+        <li class="dropdown">
+          <a href="#" class="drpbtn">Services <i class="fa fa-caret-down"></i></a>
+            <div class="drpmenu">
               <a href="room.php">Room</a>
               <a href="house.php">House</a>
               <a href="apartments.php">Apartment</a>
-            </div> -->
+            </div>
         <li><a href="contact.php">Contact Us</a></li>
         
         <li><a href="login.php">Login</a></li>
