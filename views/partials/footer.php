88ee0ebdbca5564819989fd42412a3defc3233cd
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-10-17 16:25:19 -0700

    Create footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,4 +1,34 @@
 </div>
+ <!-- Footer -->
+ <footer>
+   
+    
+    <!-- Contact Info -->
+    <section class="contactInfo">
+      <article>
+        <h4>Contact Information</h4>
+        <ul>
+          <li>Phone: 250-000-0000</li>
+          <li>Email: info@cvrn.com</li>
+          <li>Address: 1015, Cumberland Road, Courtenay</li>
+        </ul>
+        <p class="copyright">CVRN &copy;2023</p>
+      </article>
+    </section>
 
+    <!-- Social Media -->
+    <section class="social">
+      <article>
+        <h3>Let's be social - connect with us!</h3>
+        <div class="socialIcons">
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+          <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+      </article>
+    </section>
+  </footer>
 </body>
 </html>
--- views/partials/head.php
+++ views/partials/head.php
@@ -4,7 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Comox Valley Realty Navigator</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/favicon.png">
+    <!-- load font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!-- Link style sheet -->
     <link rel="stylesheet" type="text/css" href="style.css">    
+    <!-- Google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 </head>
 <body >
 
