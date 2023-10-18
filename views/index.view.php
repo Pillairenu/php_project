12740b543091589446b03264c1c9dd6a12712754
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-10-17 18:09:34 -0700

    Add a hero image in index page

--- views/index.view.php
+++ views/index.view.php
@@ -4,10 +4,21 @@
 <?php require('partials/nav.php');?>
 
   <main>
-    <div >
-      
-      <p>  </p>
+    <!-- <div >
+     
+      <section class="banner" id="banner_image">
+        <img src="images/real-estate-5.jpg" alt=" Home rental image">
+      </section>
+    </div> -->
+    <div class="hero-banner">
+        <img src="images/real-estate-6.jpg" alt="Home rental image" class="hero-image">
+        <div class="hero-text">
+            <h1>Welcome to Comox Valley Realty Navigator</h1>
+            
+        </div>
     </div>
+
+
   </main>
   
   <?php require('partials/footer.php');?>
