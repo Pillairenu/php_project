6c3e573dae054c9d20565099f0fda8460b8027c2
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-10-17 19:00:49 -0700

    Add content in index page

--- views/index.view.php
+++ views/index.view.php
@@ -17,7 +17,61 @@
             
         </div>
     </div>
+    <div class="main-container">
+      <section class="introductory-content">
+        <p>Welcome to Comox Valley Realtor Navigator
+        Your Guide to Real Estate Excellence in the Comox Valley.
+        Are you ready to embark on a journey to find your dream home or make a strategic investment 
+        in the stunning Comox Valley? You've come to the right place! Comox Valley Realtor Navigator is your 
+        trusted partner on this exciting real estate adventure.</p>
 
+        <h2>Discover the Comox Valley</h2>
+
+        <p>The Comox Valley is a hidden gem nestled on the east coast of Vancouver Island, 
+          where lush forests meet the crystal waters of the Pacific Ocean. Whether you're searching 
+          for a picturesque family home, a tranquil retreat, or a savvy investment opportunity, we've got you covered.</p>
+
+      <h2>Why Choose Us?</h2>
+
+       <p> Our team of experienced real estate professionals knows the Comox Valley inside and out.
+        We're here to provide you with the knowledge and insights you need.
+        We understand that every client's needs are unique. We'll work closely with you to tailor our services to meet your real estate goals.
+        Beyond just buying and selling, we're deeply connected to the Comox Valley community.
+        We'll introduce you to the local lifestyle and help you become part of this welcoming region.</p>
+
+        <h2>Your Real Estate Journey Begins Here:</h2>
+
+        <p>Navigate your way through our extensive listings, explore valuable resources on 
+        the local market, and get to know the vibrant communities that make up the Comox Valley. 
+        Whether you're a first-time homebuyer, a seasoned investor,renter or simply in search of your next adventure, 
+        Comox Valley Realtor Navigator is here to guide you every step of the way.</p>
+      </section>
+      <article class="article-container">
+        <div class="image">
+            <img src="images/buy_home.png" alt="Image">
+        </div>
+        <div class="content">
+            <h2>Buy or Rent a Home in the Comox Valley</h2>
+            <p>At Comox Valley Realtor Navigator, we understand that the decision to buy or rent a home is a significant one,
+               and it's unique to each individual's circumstances and preferences. The stunning Comox Valley offers a diverse 
+               range of real estate opportunities, 
+              and whether you're drawn to the idea of homeownership or the flexibility of renting, we're here to guide you.</p>
+        </div>
+    </article>
+    <article class="article-container">
+    
+        <div class="content">
+            <h2>Apartment or Condo in the Comox Valley</h2>
+            <p>At Comox Valley Realtor Navigator, we recognize that the choice between an apartment or a condo is a significant one,
+               with each option offering distinct advantages.
+               As you explore the captivating Comox Valley, you'll discover a variety of living spaces, 
+               and we're here to help you make the right decision.</p>
+        </div>
+        <div class="image">
+            <img src="images/apartment_condo.jpg" alt="Image">
+        </div>
+    </article>
+    </div>
 
   </main>
   
