0d42342a765c67ae9737b9e2d0a73f85592f77fa
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-10-25 11:20:36 -0700

    Add new form field user type in signup form

--- views/signup.view.php
+++ views/signup.view.php
@@ -11,12 +11,20 @@
             <h2>Sign Up</h2>
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" required>
+             
+                <!-- User Type Selection -->
+                <label for="user-type">User Type:</label>
+                <select id="user-type" name="user-type" required>
+                    <option value="customer" selected>Property Seeker</option>
+                    <option value="real-estate-agent">Real Estate Agent</option>
+                </select>
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required>
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
             <label for="confirm-password">Confirm Password:</label>
             <input type="password" id="confirm-password" name="confirm-password" required>
+          
             <button type="submit">Sign Up</button>
         </form>
     </div>
