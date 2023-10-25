80f1772117fd7b1e125650f0f9b4eb64ee8f217b
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-10-25 12:01:36 -0700

    Add constants as route path

---
+++ constants.php
@@ -0,0 +1,13 @@
+<?php
+
+define('ROOT', '/');
+define('SIGNIN', '/signin');
+define('SIGNUP', '/signup');
+define('ROOMS', '/rooms');
+define('HOUSE', '/house');
+define('APT', '/apartment');
+define('DETAILS',  '/room-details');
+define('FAV',  '/favorite');
+define('PROPERTY',  '/property');
+define('BOOK',  '/bookings');
+?>
