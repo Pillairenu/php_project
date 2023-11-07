b3559cbb07ded06d1e4c023c5055063a3f183ab1
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-11-07 12:32:44 -0800

    Implement secure user sign in and authentication

--- model/signin.model.php
+++ model/signin.model.php
@@ -2,4 +2,45 @@
 
 require_once('../Database.php');
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Hash the provided password
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Query the database to find a user with the provided username
+    $query = "SELECT id, username, pass FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+
+    // Execute the statement
+    $stmt->execute();
+
+    // Fetch the results
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result) {
+        $storedPassword = $result['pass'];
+
+        // Compare the provided password's hash with the stored password hash
+        if (password_verify($password, $storedPassword)) {
+            // User is authenticated, perform necessary actions (e.g., set up a session)
+            session_start();
+            $_SESSION['user_id'] = $result['id'];
+            // Redirect to the user's dashboard or display a success message
+            header("Location: /");
+            exit();
+        } 
+        
+        else {
+            echo "Invalid password.";
+        }
+    } 
+    
+    else {
+        echo "User not found.";
+    }
+}
 ?>
--- router.php
+++ router.php
@@ -5,35 +5,35 @@ $uri =parse_url($_SERVER['REQUEST_URI'])['path'];
 // Use Constants for Route Paths
 require 'constants.php';
 
-$routes=[
-
-    ROOT =>'controllers/index.php',
-    SIGNIN =>'controllers/signin.php',
-    SIGNUP=>'controllers/signup.php',
-    ROOMS=>'controllers/rooms.php',
-    HOUSE=>'controllers/house.php',
-    APT=>'controllers/apartment.php',
-    DETAILS=>'controllers/room-details.php',
-    FAV=>'controllers/favorite.php',
-    PROPERTY=>'controllers/add_property.php',
-    BOOK=>'controllers/view_bookings.php'
-];
-
 // $routes=[
 
-//     '/'=>'controllers/index.php',
-//     '/signin'=>'controllers/signin.php',
-//     '/signup'=>'controllers/signup.php',
-//     '/rooms'=>'controllers/rooms.php',
-//     '/house'=>'controllers/house.php',
-//     '/apartment'=>'controllers/apartment.php',
-//     '/room-details'=>'controllers/room-details.php',
-//     '/favorite'=>'controllers/favorite.php',
-//     '/property'=>'controllers/add_property.php',
-//     '/bookings'=>'controllers/view_bookings.php'
-
-
+//     ROOT =>'controllers/index.php',
+//     SIGNIN =>'controllers/signin.php',
+//     SIGNUP=>'controllers/signup.php',
+//     ROOMS=>'controllers/rooms.php',
+//     HOUSE=>'controllers/house.php',
+//     APT=>'controllers/apartment.php',
+//     DETAILS=>'controllers/room-details.php',
+//     FAV=>'controllers/favorite.php',
+//     PROPERTY=>'controllers/add_property.php',
+//     BOOK=>'controllers/view_bookings.php'
 // ];
+
+$routes=[
+
+    '/'=>'controllers/index.php',
+    '/signin'=>'controllers/signin.php',
+    '/signup'=>'controllers/signup.php',
+    '/rooms'=>'controllers/rooms.php',
+    '/house'=>'controllers/house.php',
+    '/apartment'=>'controllers/apartment.php',
+    '/room-details'=>'controllers/room-details.php',
+    '/favorite'=>'controllers/favorite.php',
+    '/property'=>'controllers/add_property.php',
+    '/bookings'=>'controllers/view_bookings.php'
+
+
+];
 
 
 
