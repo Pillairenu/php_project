939a0055f1b264c88e0eb834812aba51cf2d1907
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-11-07 13:02:06 -0800

    Store user type in session variable

--- model/signin.model.php
+++ model/signin.model.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     // Query the database to find a user with the provided username
-    $query = "SELECT id, username, pass FROM users WHERE username = :username";
+    $query = "SELECT id, username, pass, user_type FROM users WHERE username = :username";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
 
@@ -29,6 +29,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // User is authenticated, perform necessary actions (e.g., set up a session)
             session_start();
             $_SESSION['user_id'] = $result['id'];
+            $_SESSION['user_type'] = $result['user_type']; // Store user_type in the session
+            // var_dump($_SESSION);
             // Redirect to the user's dashboard or display a success message
             header("Location: /");
             exit();
