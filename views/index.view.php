7bf6a00eb5ef5c86b72216f24d6801a30989ea0d
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-11-28 15:29:03 -0800

    Add media query for footer

--- views/index.view.php
+++ views/index.view.php
@@ -4,12 +4,7 @@
 <?php require('partials/nav.php');?>
 
   <main>
-    <!-- <div >
-     
-      <section class="banner" id="banner_image">
-        <img src="images/real-estate-5.jpg" alt=" Home rental image">
-      </section>
-    </div> -->
+   
     <div class="hero-banner">
         <img src="images/hero-1.jpg" alt="Home rental image" class="hero-image">
         <div class="hero-text">
